629b9d541c956a3049c70b2bdfe9365372a44ca4
alksvx <br.kseny@ya.ru>
2023-07-07 11:32:05 +0300

    update main.py

--- main.py
+++ main.py
@@ -13,11 +13,7 @@ def starter(message):
     letsgo = types.ReplyKeyboardMarkup(resize_keyboard=True)
     lets = types.KeyboardButton("Погнали")
     letsgo.row(lets)
-    bot.send_message(message.chat.id, "Привет, это наш маникюр-бот. Начнем?", reply_markup=letsgo)
-    bot.register_next_step_handler(message, size)
-
-
-def size(message):
+    bot.send_message(message.chat.id, "Привет, это наш маникюр-бот.", reply_markup=letsgo)
     length = types.ReplyKeyboardMarkup(resize_keyboard=True)
     short = types.KeyboardButton("Короткие")
     length.row(short)
@@ -25,10 +21,10 @@ def size(message):
     length.row(long)
 
     bot.send_message(message.chat.id, "Выбери длину", reply_markup=length)
-    bot.register_next_step_handler(message, form)
+    bot.register_next_step_handler(message, formanails)
 
 
-def form(message):
+def formanails(message):
     global forma
     forma = [message.text]
     form = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -120,11 +116,82 @@ def menuli(message):
         bot.register_next_step_handler(message, appointment)
 
     elif message.text == 'Сначала':
-        answer = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        yes = types.KeyboardButton('Да!')
-        answer.row(yes)
-        bot.send_message(message.chat.id, 'Точно?', reply_markup=answer)
-        bot.register_next_step_handler(message, size)
+            length = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            short = types.KeyboardButton("Короткие")
+            length.row(short)
+            long = types.KeyboardButton("Длинные")
+            length.row(long)
+
+            bot.send_message(message.chat.id, "Выбери длину", reply_markup=length)
+            bot.register_next_step_handler(message, formanails1)
+
+def formanails1(message):
+    global forma
+    forma = [message.text]
+    form = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    square = types.KeyboardButton("Квадратная")
+    form.row(square)
+    almond = types.KeyboardButton("Миндалевидная")
+    form.row(almond)
+    pic = open('images/form.jpg', 'rb')
+    bot.send_photo(message.chat.id, pic)
+    bot.send_message(message.chat.id, 'Выбери форму', reply_markup=form)
+    bot.register_next_step_handler(message, color1)
+
+def color1(message):
+
+    forma.append(message.text)
+    colors = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    vivid = types.KeyboardButton("Яркий")
+    colors.row(vivid)
+    nude = types.KeyboardButton("Нюд")
+    colors.row(nude)
+
+    bot.send_message(message.chat.id,
+                     'Выбери цвет',
+                     reply_markup=colors)
+
+    bot.register_next_step_handler(message, design1)
+
+def design1(message):
+
+    forma.append(message.text)
+    design_list = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    nope = types.KeyboardButton('Без дизайна')
+    design_list.row(nope)
+    french = types.KeyboardButton('Френч')
+    design_list.row(french)
+    glitter = types.KeyboardButton("Блестки")
+    design_list.row(glitter)
+    hard = types.KeyboardButton("Сложный дизайн")
+    design_list.row(hard)
+    bot.send_message(message.chat.id, 'Выбери дизайн', reply_markup=design_list)
+
+    bot.register_next_step_handler(message, ready1)
+
+def ready1(message):
+    design1 = message.text
+    forma_str = "/".join(forma)
+    design1_str1 = ''.join(design1)
+    global allp, cpt
+
+    allp = forma_str + '.' + design1_str1
+    cpt = sum([len(files) for r, d, files in os.walk(
+        "C:\\Users\\Mikhail\\Downloads\\Нужное\\Ксюша\\Программирование\\PycharmProjects\\nails-bot\\images\\" + allp)])
+    if cpt > 1:
+        pic = open('images/' + allp + '/' + str(random.randint(1, cpt)) + '.jpg', 'rb')
+    else:
+        pic = open('images/' + allp + '/1.jpg', 'rb')
+
+    menu = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    more = types.KeyboardButton('Еще!')
+    menu.row(more)
+    want = types.KeyboardButton('Хочу такие')
+    menu.row(want)
+    again = types.KeyboardButton("Сначала")
+    menu.row(again)
+    bot.send_photo(message.chat.id, pic, reply_markup=menu)
+    bot.register_next_step_handler(message, menuli)
 
 
 def appointment(message):
@@ -132,4 +199,3 @@ def appointment(message):
 
 
 bot.polling(none_stop=True)
-
